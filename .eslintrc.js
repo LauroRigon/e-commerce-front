89e4d991683dbb6f976d199451d641161fcb2b5f
laurorigon <laurorigon@gmail.com>
2021-07-17 11:03:18 -0300

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,10 +10,68 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
+  "plugins": [
+    "vue"
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'quotes': ['error', 'double'],
-    "curly": "error"
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "no-unused-vars": [
+      "error",
+      {
+        "vars": "all",
+        "args": "after-used",
+        "ignoreRestSiblings": true
+      }
+    ],
+    "keyword-spacing": [
+      "error",
+      {
+        "before": true,
+        "after": true
+      }
+    ],
+    "curly": [
+      "error",
+      "all"
+    ],
+    "vue/html-indent": [
+      "error", 4
+    ],
+    "comma-dangle": [
+      "error",
+      {
+        "arrays": "always-multiline",
+        "objects": "always-multiline",
+        "imports": "always-multiline",
+        "exports": "always-multiline",
+        "functions": "never"
+      }
+    ],
+    "no-trailing-spaces": [
+      "error"
+    ],
+    "no-useless-return": [
+      "error"
+    ],
+    "no-else-return": [
+      "error"
+    ],
+    "padded-blocks": [
+      "error",
+      "never"
+    ]
   }
 }
